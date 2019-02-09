4e42817617c200332b04e6c180b44925d40ed9ce
Wilson <wiyu98@gmail.com>
2019-02-09 12:23:10 -0500

    Formatted and added initialization mouseScroll

--- mouseScroll.js
+++ mouseScroll.js
@@ -1,3 +1,6 @@
+//Initialize mouseScroll.
+mouseScroll();
+
 /**
  * 
  * @param scrollUpZone
@@ -18,48 +21,48 @@
  * the higher the number, the faster the scroll.
  * Default: 0.10
  */
-function mouseScroll(scrollUpZone=100,
-  scrollDownZone=100,
-  refreshInterval=20,
-  scrollDist=0.10) {
+function mouseScroll(scrollUpZone = 100,
+                    scrollDownZone = 100,
+                    refreshInterval = 20,
+                    scrollDist = 0.10) {
 
-var lowerThreshold = window.innerHeight - scrollDownZone;
-var speed = 0;
-var direction = 0;
+  var lowerThreshold = window.innerHeight - scrollDownZone;
+  var speed = 0;
+  var direction = 0;
 
-//Recalibrate the direction and speed of the scroll
-onmousemove = function (event) {
-if (event.clientY <= scrollUpZone) {
-speed = (scrollUpZone - event.clientY) * scrollDist;
-direction = -1;
-} else if (event.clientY >= lowerThreshold) {
-speed = (event.clientY - window.innerHeight + scrollDownZone) * scrollDist;
-direction = 1;
-} else {
-speed = 0;
-}
-}
+  //Recalibrate the direction and speed of the scroll
+  onmousemove = function (event) {
+    if (event.clientY <= scrollUpZone) {
+      speed = (scrollUpZone - event.clientY) * scrollDist;
+      direction = -1;
+    } else if (event.clientY >= lowerThreshold) {
+      speed = (event.clientY - window.innerHeight + scrollDownZone) * scrollDist;
+      direction = 1;
+    } else {
+      speed = 0;
+    }
+  }
 
-//Trigger the scroll
-setInterval(function() {
-window.scrollBy(0, speed * direction);
-}, refreshInterval);
+  //Trigger the scroll
+  setInterval(function () {
+    window.scrollBy(0, speed * direction);
+  }, refreshInterval);
 
-//Stop scrolling when the mouse leaves the window
-document.addEventListener("mouseleave", function () {
-decelerate();
-});
+  //Stop scrolling when the mouse leaves the window
+  document.addEventListener("mouseleave", function () {
+    decelerate();
+  });
 
-//Smoothly stop scrolling
-function decelerate() {
-var decelerate = setInterval(function () {
-if (speed > 0.5) {
-speed = speed - 0.5;
-}
-else if (speed > 0) {
-speed = 0;
-clearTimeout(decelerate);
-}
-}, refreshInterval);
-}
+  //Smoothly stop scrolling
+  function decelerate() {
+    var decelerate = setInterval(function () {
+      if (speed > 0.5) {
+        speed = speed - 0.5;
+      }
+      else if (speed > 0) {
+        speed = 0;
+        clearTimeout(decelerate);
+      }
+    }, refreshInterval);
+  }
 }
